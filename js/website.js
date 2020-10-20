a20c19673b0330388f6d687a192690e4f089db86
Saeen Damghanian <saeen.damghanian@gmail.com>
2020-10-20 14:59:08 +0200

    End of Discount Code page

--- js/website.js
+++ js/website.js
@@ -93,3 +93,10 @@ inputs.forEach((input) => {
 });
 
 /* Form scripts ends */
+
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
