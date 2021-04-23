a1fcc3ba559fcd90f5167269946d44e4b5bec110
Saeen Damghanian <saeendamghanian@Saeens-MacBook-Pro.local>
2021-04-23 16:52:06 +0430

    Add cookie popup alert

--- js/website.js
+++ js/website.js
@@ -9,61 +9,58 @@
 
 /* Set the width of the side navigation to 250px */
 function openNav() {
-  document.getElementById("sidenav-items").style.width = "250px";
+	document.getElementById('sidenav-items').style.width = '250px';
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
-  document.getElementById("sidenav-items").style.width = "0";
+	document.getElementById('sidenav-items').style.width = '0';
 }
 
 // When the user scrolls down 20px from the top of the document, slide down the navbar
 // When the user scrolls to the top of the page, slide up the navbar (50px out of the top view)
 window.onscroll = function () {
-  scrollFunction();
+	scrollFunction();
 };
 
 function scrollFunction() {
-  const headerElement = document.querySelector("header");
-  const headerHeight = headerElement.getBoundingClientRect().height;
-  const contactElement = document.querySelector('#contact');
+	const headerElement = document.querySelector('header');
+	const headerHeight = headerElement.getBoundingClientRect().height;
+	const contactElement = document.querySelector('#contact');
 
-  if (
-    document.body.scrollTop > 100 ||
-    document.documentElement.scrollTop > 100
-  ) {
-    headerElement.style.top = `${-1 * headerHeight + 7}px`;
-  } else {
-    headerElement.style.top = "0";
-  }
+	if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+		headerElement.style.top = `${-1 * headerHeight + 7}px`;
+	} else {
+		headerElement.style.top = '0';
+	}
 
-  if (contactElement) {
-    const contactHeight = contactElement.getBoundingClientRect().y;
+	if (contactElement) {
+		const contactHeight = contactElement.getBoundingClientRect().y;
 
-    if (contactHeight - 50 < 0 ) {
-      activeContact();
-    } else {
-      diactiveContact();
-    }
-  }
+		if (contactHeight - 50 < 0) {
+			activeContact();
+		} else {
+			diactiveContact();
+		}
+	}
 }
 
 function showHeader() {
-  document.querySelector("header").style.top = "0px";
+	document.querySelector('header').style.top = '0px';
 }
 
 function activeContact() {
-  const navItems = document.querySelectorAll('.nav-item');
+	const navItems = document.querySelectorAll('.nav-item');
 
-  navItems[0].classList.remove('current-page');
-  navItems[navItems.length - 1].classList.add('current-page');
+	navItems[0].classList.remove('current-page');
+	navItems[navItems.length - 1].classList.add('current-page');
 }
 
 function diactiveContact() {
-  const navItems = document.querySelectorAll('.nav-item');
-  
-  navItems[0].classList.add('current-page');
-  navItems[navItems.length - 1].classList.remove('current-page');
+	const navItems = document.querySelectorAll('.nav-item');
+
+	navItems[0].classList.add('current-page');
+	navItems[navItems.length - 1].classList.remove('current-page');
 }
 
 /* 
@@ -72,14 +69,27 @@ function diactiveContact() {
 
 /* Form scripts starts */
 
-const inputs = document.querySelectorAll("input");
+const inputs = document.querySelectorAll('input');
 
-inputs.forEach((input) => {
-  input.addEventListener("change", (event) => {
-    if (event.target.value) event.target.classList.add("value-changed");
-    else event.target.classList.remove("value-changed");
-  });
+inputs.forEach(input => {
+	input.addEventListener('change', event => {
+		if (event.target.value) event.target.classList.add('value-changed');
+		else event.target.classList.remove('value-changed');
+	});
 });
 
 /* Form scripts ends */
 
+/* Cookie */
+const cookieBanner = document.querySelector('.cookie-banner');
+
+if (localStorage.getItem('cookieSeen') === 'shown') {
+	localStorage.setItem('cookieSeen', 'not shown');
+}
+if (localStorage.getItem('cookieSeen') !== 'shown') {
+	cookieBanner.style.display = 'flex';
+	localStorage.setItem('cookieSeen', 'shown');
+}
+document.querySelector('.close').addEventListener('click', function () {
+	cookieBanner.style.display = 'none';
+});
